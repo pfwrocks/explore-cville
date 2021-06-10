5e2b93a3d4266cf71fe894c85c9301e98b3d5a32
cab8xd <49292117+cab8xd@users.noreply.github.com>
2021-06-10 17:41:29 -0400

    Update db-add.php

--- db-add.php
+++ db-add.php
@@ -1,12 +1,22 @@
 <?php 
 require('connect-db.php');
 
-// Add activity functional on 'all' tab on activities.php
+
+// Helper functon that returns the latest activity ID
+function getNewActivitiesID(){
+    global $db;
+    $query = "SELECT * FROM ACTIVITY";
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $results = $statement->fetchAll();
+    $statement->closeCursor();
+    return sizeof($results);
+}
+// Add activity functional on 'all' tab, activities.php
 # Likely needs
 # - Form validation, specifically with the date inputs since those are picky.
 # - to be more pretty. 
-# but maybe that's not necessarly all required b/c this is a db not a web course -_-
-
+# but maybe she won't grade hard because this is a db not a web course -_-
 # Form that adds an activity into the database's ACTIVITY table. 
 function addActivityForm()
 {
@@ -56,4 +66,75 @@ function addActivity($name, $open, $close, $type, $url)
     $statement->closeCursor();
 }
 
+
+
+function addHikeForm()
+{
+    echo"<p> Add activity </p>";
+    echo "<html>
+        <body>
+        <form action = 'activities.php?btnaction=hike' method='post'>
+        Name: <input type='text' name='activity_name'><br>
+        Opens: <input type='text' name='activity_opentime'><br>
+        Closes: <input type='text' name='activity_closetime'><br>
+        Type: <input type='text' name='activity_type'><br>
+        URL: <input type='text' name='activity_url'><br>
+        <br>
+        Difficulty: <input type='text' name='hike_difficulty'><br>
+        Length: <input type='text' name='hike_length'><br>
+        Topological gain: <input type='text' name='hike_topo'><br>
+        Street address: <input type='text' name='hike_st'><br>
+        Zip: <input type='text' name='hike_zip'><br>
+        <input type='submit'>
+    </form>
+        </body>
+    </html>";
+
+    addActivity($_POST['activity_name'], 
+        $_POST['activity_opentime'],
+        $_POST['activity_closetime'],
+        $_POST['activity_type'],
+        $_POST['activity_url']);
+    addHike(
+        getNewActivitiesID(),
+        $_POST['activity_name'],
+        $_POST['hike_difficulty'], 
+        $_POST['hike_length'],
+        $_POST['hike_topo'],
+        $_POST['hike_st'],
+        $_POST['hike_zip']);
+        
+}
+function addHike($id, $name, $diff, $len, $top, $st, $zip)
+{
+    # example sql
+    /*
+    INSERT INTO `HIKE` (`ACTIVITY_ID`, `HIKE_NAME`, `HIKE_DIFFICULTY`, `HIKE_LENGTH`, `HIKE_TOPO_GAIN`, `HIKE_STREET`, `HIKE_ZIP`) VALUES
+    (1, 'Old Rag Mountain Loop', 'Hard', '9.5', '2683', 'State Rte 600, Etlan, VA', '22719'),
+    (2, 'Humpback Rocks Hike', 'Moderate', '4.0', '1240', 'Humpback Gap Overlook, Afton, VA', '22920'),
+    (3, 'Riprap Trail', 'Hard', '9.3', '2116', 'Wildcat Ridge Parking Area, Crimora, VA', '24431');
+    */
+    global $db;
+    $query = 
+    "INSERT INTO HIKE
+        (ACTIVITY_ID, 
+        HIKE_NAME, 
+        HIKE_DIFFICULTY, 
+        HIKE_LENGTH, 
+        HIKE_TOPO_GAIN,
+        HIKE_STREET,
+        HIKE_ZIP) 
+    VALUES (:id, :name, :diff, :len, :top, :st, :zip)";
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':id', $id);
+    $statement->bindValue(':diff', $diff);
+    $statement->bindValue(':len', $len);
+    $statement->bindValue(':top', $top);
+    $statement->bindValue(':st', $st);
+    $statement->bindValue(':zip', $zip);
+    $statement->execute();
+    $statement->closeCursor();
+}
 ?>
