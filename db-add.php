b6fff62b9894d97be3637a99579901ad741159ea
cab8xd <49292117+cab8xd@users.noreply.github.com>
2021-06-10 21:32:28 -0400

    Update db-add.php

--- db-add.php
+++ db-add.php
@@ -2,7 +2,7 @@
 require('connect-db.php');
 
 
-// Helper functon that returns the latest activity ID
+// Helper function that returns the latest activity ID
 function getNewActivitiesID(){
     global $db;
     $query = "SELECT * FROM ACTIVITY";
@@ -90,19 +90,22 @@ function addHikeForm()
         </body>
     </html>";
 
-    addActivity($_POST['activity_name'], 
-        $_POST['activity_opentime'],
-        $_POST['activity_closetime'],
-        $_POST['activity_type'],
-        $_POST['activity_url']);
-    addHike(
-        getNewActivitiesID(),
-        $_POST['activity_name'],
-        $_POST['hike_difficulty'], 
-        $_POST['hike_length'],
-        $_POST['hike_topo'],
-        $_POST['hike_st'],
-        $_POST['hike_zip']);
+    if(isset($_POST['activity_name']))
+    {
+        addActivity($_POST['activity_name'], 
+            $_POST['activity_opentime'],
+            $_POST['activity_closetime'],
+            $_POST['activity_type'],
+            $_POST['activity_url']);
+        addHike(
+            getNewActivitiesID(),
+            $_POST['activity_name'],
+            $_POST['hike_difficulty'], 
+            $_POST['hike_length'],
+            $_POST['hike_topo'],
+            $_POST['hike_st'],
+            $_POST['hike_zip']);
+    }
         
 }
 function addHike($id, $name, $diff, $len, $top, $st, $zip)
@@ -132,6 +135,70 @@ function addHike($id, $name, $diff, $len, $top, $st, $zip)
     $statement->bindValue(':diff', $diff);
     $statement->bindValue(':len', $len);
     $statement->bindValue(':top', $top);
+    $statement->bindValue(':st', $st);
+    $statement->bindValue(':zip', $zip);
+    $statement->execute();
+    $statement->closeCursor();
+}
+
+
+// Form for inserting new restaurants. 
+function addRestaurantForm(){
+    echo"<p> Add activity </p>";
+    echo "<html>
+        <body>
+        <form action = 'activities.php?btnaction=hike' method='post'>
+        Name: <input type='text' name='activity_name'><br>
+        Opens: <input type='text' name='activity_opentime'><br>
+        Closes: <input type='text' name='activity_closetime'><br>
+        Type: <input type='text' name='activity_type'><br>
+        URL: <input type='text' name='activity_url'><br>
+        <br>
+        Rating: <input type='text' name='res_rating'><br>
+        Price: <input type='text' name='res_price'><br>
+        Cuisine: <input type='text' name='res_cuisine'><br>
+        Street address: <input type='text' name='res_st'><br>
+        Zip: <input type='text' name='res_zip'><br>
+        <input type='submit'>
+    </form>
+        </body>
+    </html>";
+    if(isset($_POST['activity_name']))
+    {
+        addActivity($_POST['activity_name'], 
+            $_POST['activity_opentime'],
+            $_POST['activity_closetime'],
+            $_POST['activity_type'],
+            $_POST['activity_url']);
+        addRestaurant(
+            getNewActivitiesID(),
+            $_POST['activity_name'],
+            $_POST['res_rating'], 
+            $_POST['res_price'],
+            $_POST['res_cuisine'],
+            $_POST['res_st'],
+            $_POST['res_zip']);
+    }
+}
+function addRestaurant($id, $name, $rate, $pr, $cui, $st, $zip){
+    global $db;
+    $query = 
+    "INSERT INTO RESTAURANT
+        (ACTIVITY_ID,
+        RESTAURANT_NAME,	
+        RESTAURANT_RATING,	
+        RESTAURANT_PRICE_RANGE,	
+        RESTAURANT_CUISINE,
+        RESTAURANT_STREET,	
+        RESTAURANT_ZIP) 
+    VALUES (:id, :name, :rate, :pr, :cui, :st, :zip)";
+
+    $statement = $db->prepare($query);
+    $statement->bindValue(':name', $name);
+    $statement->bindValue(':id', $id);
+    $statement->bindValue(':rate', $rate);
+    $statement->bindValue(':pr', $pr);
+    $statement->bindValue(':cui', $cui);
     $statement->bindValue(':st', $st);
     $statement->bindValue(':zip', $zip);
     $statement->execute();
