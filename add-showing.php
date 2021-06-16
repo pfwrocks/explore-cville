787d9bfe21c6da2ab0122dea25b0b1630190c602
Preston Wright <54689879+pfwrocks@users.noreply.github.com>
2021-06-15 23:43:18 -0400

    Update add-showing.php

--- add-showing.php
+++ add-showing.php
@@ -1,24 +1,78 @@
+<?php include './components/navigation.php';?>
+
+  <!-- Masthead-->
+  <header class="masthead bg-primary text-white text-center">
+      <div class="container d-flex align-items-center flex-column">
+          <!-- Masthead Heading-->
+
+          <h1 class="masthead-heading text-uppercase mb-0">Add Showing</h1>
+      </div>
+  </header>
+
+  <div class="container">
+    <center>
+    <div class="row">
+      <div class="col-1" style="line-height:75px"> </div>
+      <div class="col-1" style="line-height:75px"> </div>
+      <div class="col-9">
+        <br>
+        <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post" style="line-height:50px">
+
 <?php
 /*
 example input
 INSERT INTO SHOWTIME (ACTIVITY_ID, THEATER_ID, SHOW_TIME) VALUES (:activity_id, :theater_id :showtime)8 3 2021-06-02 20:15:47
 */
+<<<<<<< HEAD
+=======
 include './components/navigation.php';
+>>>>>>> 00c6de1fb99bfb1ca0bfe11e4708429f4f1a5775
 require("connect-db.php");
 addShowingForm();
 // showings.php
 function addShowingForm(){
-    echo"<p> Add a showing </p>";
-    echo "<html>
-        <body>
-        <form action = '' method='post'>
-        Activity ID#: <input type='text' name='activity_id'><br>
-        Theater ID#: <input type='text' name='theater_id'><br>
-        Showtime: <input type='text' name='showtime'><br>
-        <input type='submit'>
-    </form>
-        </body>
-    </html>";
+    global $db;
+    $query = "SELECT * FROM THEATER";
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $r1 = $statement->fetchAll();
+        $statement->closeCursor();
+    $query = "SELECT * FROM MOVIE INNER JOIN ACTIVITY ON ACTIVITY.ACTIVITY_ID=MOVIE.ACTIVITY_ID";
+        $statement = $db->prepare($query);
+        $statement->execute();
+        $r2 = $statement->fetchAll();
+        $statement->closeCursor();
+    
+    echo"        
+        <div class='container'>
+        
+        <div class='input-group mb-3'>
+            <span class='input-group-text' id='basic-addon3' style='width:15%'>Movie</span>
+            <select name='activity_id' class='form-select form-select-lg mb-3' aria-label='.form-select-lg example'>";
+            foreach ($r2 as $rs2)
+                    echo "<option value='" . $rs2['ACTIVITY_ID'] . "'>".$rs2['ACTIVITY_NAME']."</option>";
+    echo "
+        </select></div>
+        
+        <div class='input-group mb-3'>
+            <span class='input-group-text' id='basic-addon3' style='width:15%'>Theater</span>
+        <select name='theater_id' class='form-select form-select-lg mb-3' aria-label='.form-select-lg example'>";
+            foreach ($r1 as $rs1)
+                    echo "<option value='" . $rs1['THEATER_ID'] . "'>".$rs1['THEATER_NAME']."</option>";
+    echo "</select></div>
+
+        <div class='input-group mb-3'>
+            <span class='input-group-text' id='basic-addon3' style='width:15%'>Show Time</span>
+            <input name='showtime' type='text' class='form-control' id='basic-url' aria-describedby='basic-addon3' 
+                value=''>
+        </div>
+        
+        </div>
+        <div class='d-grid gap-2'>
+            <button class='btn btn-primary' type='submit'>Update</button>
+        </div>
+    ";
+            
     if(isset($_POST['activity_id']))
     {
         addShowing(
@@ -31,18 +85,24 @@ function addShowingForm(){
 function addShowing($activity_id, $theater_id, $showtime){
     global $db;
     $query = 
-    "INSERT INTO SHOWTIME
+    "INSERT INTO SHOWING
         (ACTIVITY_ID,
         THEATER_ID,		
         SHOW_TIME)
-    VALUES (:activity_id, :theater_id :showtime)";
-
+    VALUES (". $activity_id .", ". $theater_id .", '". $showtime ."')";
+    
+    echo $query;
     $statement = $db->prepare($query);
-    $statement->bindValue(':activity_id', $activity_id);
-    $statement->bindValue(':theater_id', $theater_id);
-    $statement->bindValue(':showtime', $showtime);
+    // $statement->bindValue(':activity_id', $activity_id);
+    // $statement->bindValue(':theater_id', $theater_id);
+    // $statement->bindValue(':showtime', $showtime);
     $statement->execute();
     $statement->closeCursor();
 }
 
 ?>
+
+</form>
+<br /> 
+    </div>
+</div>
